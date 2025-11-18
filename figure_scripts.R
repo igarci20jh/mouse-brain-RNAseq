5748525119d07b841f83382c78306ae3c8b6a7ca
igarci20jh <igarci20@jh.edu>
2025-11-18 10:53:13 -0500

    add updated script

--- figure_scripts.R
+++ figure_scripts.R
@@ -1,14 +1,20 @@
 library(Seurat)
 library(SeuratDisk)
 library(ggplot2)
+library(zellkonverter)
+
 setwd("/Users/cmdb/qb25project/mouse-brain-RNAseq/")
 
 # Read .h5ad with low memory usage
 Convert("adata_for_seurat.h5ad", dest = "h5seurat", overwrite = TRUE)
-sce <- LoadH5Seurat("adata_for_seurat.h5seurat")
+sce <- LoadH5Seurat("adata_for_seurat.h5seurat", meta.data = FALSE, misc = FALSE)
+#sce <- readH5AD("adata_combined_nodoublet_normalized.h5ad")
 
-# Running PCA for UMAP
-sce <- RunPCA(sce)
+ # Running PCA for UMAP
+
+sce <- FindVariableFeatures(sce, selection.method = "vst", nfeatures = 2000)
+sce <- RunPCA(sce, assay = "RNA", verbose = TRUE)
+
 
 # Sckree plot to find number of PCs that explain data (used Seurat function)
 ElbowPlot(sce)
@@ -21,7 +27,7 @@ sce <- RunUMAP(sce, dims = 1:10)  # will adjust once we can read in the data
 # Plotting UMAP
 umap_df <- as.data.frame(Embeddings(sce, "umap"))
 
-# color by clusters
+# Color by clusters
 umap_df$cluster <- Idents(sce)  
 
 # Plotting UMAP
